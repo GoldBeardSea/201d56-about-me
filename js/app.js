02653c2b9584f636b13e9eb8307574ff9f345d3e
Timothy Joseph Busch <timbusch1@gmail.com>
2019-03-26 15:23:45 -0700

    scaffolded out html will be working on js next

---
+++ js/app.js
@@ -0,0 +1,7 @@
+'use strict'
+
+var questionCorrect = 0; 
+var arrayYes = ['YES', 'Y', 'YEAH', 'TRUE', 'T'];
+
+var userName = prompt('Can I catch your name first?');
+alert('Alright ' + userName + ' let\'s get to the game.');

4a9d0525ef6362174fb2541ca029d49aa1b5a84d
Timothy Joseph Busch <timbusch1@gmail.com>
2019-03-26 22:04:59 -0700

    need to commit css changes

--- js/app.js
+++ js/app.js
@@ -2,9 +2,14 @@
 
 var questionCorrect = 0; 
 var arrayYes = ['YES', 'Y', 'YEAH', 'TRUE', 'T'];
+var nameArray = ['TIM', 'TIMOTHY']
 
 var userName = prompt('Can I catch your name first?');
+if (nameArray.includes(userName.toUpperCase())) {
+    alert('Oh cool my name is also Tim')
+} else {
 alert('Alright ' + userName + ' let\'s get to the game, preferrably answer using yes or no.');
+}
 
 var guessCat = prompt('Do I like cats?').toUpperCase();
 console.log(guessCat)
@@ -12,7 +17,7 @@ if (arrayYes.includes(guessCat)) {
     questionCorrect++;
     alert('Yes I really love cats');
 } else {
-    alert('You guessed incorrectly');
+    alert('You chose poorly');
 };
 
 var guessSeattle = prompt('Am I native Seattleite.').toUpperCase();
@@ -20,7 +25,7 @@ if (arrayYes.includes(guessSeattle)) {
     questionCorrect++;
     alert('I am in fact a native Seattleite, good job')
 } else {
-    alert('All hair king of the losers - Age of Empires II')
+    alert('All hair king of the losers - Age of Empires II, we are savage here.')
 }
 
 var guessLacross = prompt('Have I played Lacross?').toUpperCase();
@@ -28,7 +33,7 @@ if (arrayYes.includes(guessLacross)) {
     alert('You got it wrong');
 } else {
     questionCorrect++;
-    alert('I have never, ever played Lacross')
+    alert('I have never, ever played Lacross, good guess')
 }
 
 var guessClimber = prompt('Have I ever climbed Mt Rainier?').toUpperCase();
@@ -56,7 +61,7 @@ if (arrayYes.includes(guessLanguage)) {
 }
 
 if (questionCorrect >= 3) {
-    alert('Congratuations you got half or more right with ' +questionCorrect+ ' out of 6 wrong')
+    alert('Congratuations you got half or more right with ' +questionCorrect+ ' out of 6')
 } else {
-    alert('Oof you got more than half wrong with ' +questionCorrect+ " out of 6")
+    alert('Oof you got more than half wrong with ' +questionCorrect+ ' out of 6')
 }
